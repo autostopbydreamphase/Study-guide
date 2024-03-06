8245ed4b728c17cd19f7f702f79fe6a255b6a343
autostopbydreamphase <kidisafraidtobealone@gmail.com>
2024-03-06 11:14:01 +0300

    npm modules

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-arr.forEach(function (item) {
-  console.log(item);
+window.sr = ScrollReveal({
+  distance: "60px",
+  duration: 800,
+  delay: 200,
+  reset: true,
 });
+sr.reveal(".left", { delay: 200, origin: "right" });
+sr.reveal(".right", { delay: 200, origin: "left" });
